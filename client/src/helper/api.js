3a52ae5f9970b07aeec76ee3a071e678c2de4533
nguyendu <nguyendu334@gmail.com>
2026-07-16 14:48:10 +0700

    edit base URL call api

--- client/src/helper/api.js
+++ client/src/helper/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.8.93:5000/api",
+  baseURL: `${window.location.protocol}//${window.location.hostname}:5000/api`,
 });
 
 api.interceptors.request.use(
